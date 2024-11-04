e3a5b0a06f8893136bdcd0f769963754b92e88b8
lebuinhatduy <lebuinhatduy@gmail.com>
2024-11-04 11:26:40 +0700

    Add project files.

---
+++ Bai1_HelloWord.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello Word!\n";
+	cout << "Toi ten la"<< "\tle bui nhat duy" << endl;
+	cout << "toi dang hoc \tc++ " << endl;
+		return 0; 
+}
